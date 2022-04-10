77d9ed0b0c2cb4e4b2e170429b43ae3a65c3f382
Dylan Tettarasar <dtettarasar@gmail.com>
2022-04-10 11:18:08 +0200

    eau00 : update in numToArr + added Bubble sort func

--- eau00.js
+++ eau00.js
@@ -7,10 +7,18 @@ const numToArr = (number, digit) => {
 
   const numArr = number.toString().split('');
 
+
+  for (let i = 0; i < numArr.length; i++) {
+    numArr[i] = parseInt(numArr[i]);
+  }
+
   if (numArr.length < digit) {
 
-    for (let i = 0; i <= digit - numArr.length; i++) {
-      numArr.unshift('0');
+    numArrInitLength = numArr.length;
+
+    for (let i = 0; i < digit - numArrInitLength; i++) {
+      // console.log(i)
+      numArr.unshift(0);
     }
 
   }
@@ -19,8 +27,47 @@ const numToArr = (number, digit) => {
 
 }
 
+//tri à bulle
+const bubbleSort = (array) => {
+
+    //console.log(array);
+
+    const process = () => {
+
+        let arrIsSorted = true;
+
+        for (let i = 0;i < array.length; i++) {
+
+            //console.log(array[i]);
+
+            if (array[i] > array[i + 1]) {
+
+                arrIsSorted = false;
+
+                let smaller = array[i + 1];
+                let higher = array[i];
+
+                array[i] = smaller;
+                array[i + 1] = higher;
+
+            }
+
+            if (!arrIsSorted) {
+                process();
+            }
+
+        }
+
+    }
+
+    process();
+
+    return array;
+
+}
 
 const main = () => {
+
 
   for (let i = 0; i < 1000; i++) {
     numToArr(i,3);
