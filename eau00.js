7c2bfc4a2dd7cb8186e44cd6d4232a57984a556e
Dylan Tettarasar <dtettarasar@gmail.com>
2022-04-10 02:55:03 -0400

    eau00 : created getAllArr and main funcs

--- eau00.js
+++ eau00.js
@@ -1,1 +1,36 @@
 // Combinaisons de 3 chiffres
+
+// functions
+
+const getAllArr = () => {
+
+    const mainArr = [];
+
+    for (let i = 0; i < 1000; i++) {
+        const numArr = i.toString().split('');
+
+        if (numArr.length == 1) {
+            for (let j = 0; j < 2;j++) {
+                numArr.unshift('0');
+            }
+        } else if (numArr.length == 2) {
+            numArr.unshift('0');
+        }
+
+        mainArr.push(numArr);
+    }
+
+    for (let i = 0; i < mainArr.length; i++) {
+        console.log(mainArr[i]);
+    }
+
+}
+
+
+const main = () => {
+
+    getAllArr();
+
+}
+
+main();
