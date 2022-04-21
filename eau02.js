bcacc57b1ed4de113469c958c432305a3bdd6a76
Dylan Tettarasar <dtettarasar@gmail.com>
2022-04-21 11:42:34 -0400

    eau02.js created displayArgs func + update in main func

--- eau02.js
+++ eau02.js
@@ -3,18 +3,33 @@
 const argTester = () => {
 
     const arguments = process.argv.slice(2);
-    console.log(arguments);
 
     if (arguments.length === 0) {
         console.log("Veuillez passer au moins un argument.");
-        console.log("Exemple: node eau02.js ");
+        console.log("Exemple: node eau02.js Leach A On Freak");
         return false;
+    }
+
+    return arguments;
+
+}
+
+const displayArgs = (array) => {
+
+    for(let i = array.length - 1; i >= 0; i--) {
+        console.log(array[i]);
     }
 
 }
 
 const main = () => {
 
+    const arguments = (argTester());
+
+    if (arguments) {
+        displayArgs(arguments);
+    }
+
 }
 
-argTester();
+main();
