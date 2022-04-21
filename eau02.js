04fc96f20f6a5462434d0b74cd979ebdb5f623de
Dylan Tettarasar <dtettarasar@gmail.com>
2022-04-21 11:32:30 -0400

    eau02 : update in argTester func

--- eau02.js
+++ eau02.js
@@ -5,6 +5,12 @@ const argTester = () => {
     const arguments = process.argv.slice(2);
     console.log(arguments);
 
+    if (arguments.length === 0) {
+        console.log("Veuillez passer au moins un argument.");
+        console.log("Exemple: node eau02.js ");
+        return false;
+    }
+
 }
 
 const main = () => {
