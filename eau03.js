f52d75cf3154d7a2a93ca7bb678f5822968abbc1
Dylan Tettarasar <dtettarasar@gmail.com>
2022-04-22 07:21:42 -0400

    eau03 update on argTester

--- eau03.js
+++ eau03.js
@@ -3,9 +3,11 @@
 const argTester = () => {
 
     const argument = process.argv.slice(2);
+    const pattern = /^[0-9]+$/;
+    const argIsNumber = pattern.test(argument[0]);
 
-    if (argument.length !== 1) {
-        console.log("veuillez passer un nombre entier en argument.");
+    if (argument.length !== 1 || !argIsNumber) {
+        console.log("Veuillez passer un nombre entier positif en argument.");
         console.log("Exemple: node eau03.js 3");
         return false;
     }
