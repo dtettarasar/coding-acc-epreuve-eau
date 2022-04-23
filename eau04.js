aafa2e838e46108b22d1c5116b17c00ab90843fa
Dylan Tettarasar <dtettarasar@gmail.com>
2022-04-23 18:15:32 +0200

    eau04: update in argTester

--- eau04.js
+++ eau04.js
@@ -1,9 +1,25 @@
 // Prochain nombre premier
 
-const argTester = () => {}
+const argTester = () => {
+
+    const argument = process.argv.slice(2);
+    const pattern = /^[0-9]+$/;
+    const argIsNumber = pattern.test(argument[0]);
+
+    if (argument.length !== 1 || !argIsNumber) {
+        console.log("Veuillez passer un nombre entier positif en argument.");
+        console.log("Exemple: node eau04.js 7");
+        return false;
+    }
+
+    return parseInt(argument[0]);
+
+}
 
 const isPrimeNumber = () => {}
 
 const findNextPrimeNumber = () => {}
 
 const main = () => {}
+
+console.log(argTester());
