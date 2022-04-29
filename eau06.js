6029ac163b69217e43d88c751a7a2dc34e403efe
Dylan Tettarasar <dtettarasar@gmail.com>
2022-04-29 11:27:33 -0400

    eau06: update on argTester func

--- eau06.js
+++ eau06.js
@@ -3,14 +3,17 @@
 const argTester = () => {
 
     const argument = process.argv.slice(2);
+    const pattern = /(?=.*[a-zA-Z].*[a-zA-Z])/
 
-    if (argument.length !== 1) {
+    if (argument.length !== 1 || !pattern.test(argument[0])) {
+
         console.log("Veuillez passer une chaîne de caractère en argument (avec au moins deux lettres).");
         console.log("Exemple: node eau06.js 'Pulse of the Maggots'");
+
+        return false;
     }
 
-    console.log(argument);
-
+    return argument[0];
 }
 
-argTester();
+console.log(argTester());
