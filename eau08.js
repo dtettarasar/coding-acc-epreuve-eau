cc20e840d0d6cf0aa47fa96c5e96530b57128633
Dylan Tettarasar <dtettarasar@gmail.com>
2022-05-01 21:50:49 +0200

    eau08 : update in argTester func

--- eau08.js
+++ eau08.js
@@ -4,8 +4,14 @@
 const argTester = () => {
 
     const argument = process.argv.slice(2);
+    
+    if (argument.length !== 1) {
+        console.log("Veuillez passer une chaîne de caractère en argument.");
+        console.log("Exemple: node eau08.js 'Twisted Transistor'");
+        return false;
+    }
 
-    console.log(argument);
+    console.log(argument[0]);
 
 }
 
