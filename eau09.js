2f08adf0992795c7bcb0e65ac5586ecffee02115
Dylan Tettarasar <dtettarasar@gmail.com>
2022-05-03 07:53:57 -0400

    eau09 : update in argTester

--- eau09.js
+++ eau09.js
@@ -3,17 +3,41 @@
 const argTester = () => {
 
     const arguments = process.argv.slice(2);
-
+    const exampleStr = "Exemple: node eau09.js 20 42"
     const pattern = /^-?[0-9]+$/;
     const argsAreNum = pattern.test(arguments[0]) && pattern.test(arguments[1]);
 
     if (arguments.length !== 2 || !argsAreNum) {
-        console.log("Veuillez passer deux nombres entiers différents en argument");
-        console.log("Exemple: node eau09.js 20 42");
+        console.log("Veuillez passer deux nombres entiers différents en argument.");
+        console.log(exampleStr);
         return false;
     }
 
-    console.log(arguments);
+    const numbers = {};
+    const numOne = parseInt(arguments[0]);
+    const numTwo = parseInt(arguments[1]);
+
+    // console.log(numOne, numTwo);
+
+    if (numOne === numTwo) {
+
+        console.log("Attention: Les deux nombres entiers doivent être différents.");
+        console.log(exampleStr);
+        return false;
+
+    } else if (numOne > numTwo) {
+
+        numbers.great = numOne;
+        numbers.small = numTwo;
+
+    } else {
+
+        numbers.great = numTwo;
+        numbers.small = numOne;
+
+    }
+
+    console.log(numbers);
 
 }
 
