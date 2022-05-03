4200fc1ffc7411efd51c759a7c74d57146e2f8dc
Dylan Tettarasar <dtettarasar@gmail.com>
2022-05-03 07:34:32 -0400

    eau09 : update in argTester

--- eau09.js
+++ eau09.js
@@ -4,7 +4,10 @@ const argTester = () => {
 
     const arguments = process.argv.slice(2);
 
-    if (arguments.length !== 2) {
+    const pattern = /^-?[0-9]+$/;
+    const argsAreNum = pattern.test(arguments[0]) && pattern.test(arguments[1]);
+
+    if (arguments.length !== 2 || !argsAreNum) {
         console.log("Veuillez passer deux nombres entiers différents en argument");
         console.log("Exemple: node eau09.js 20 42");
         return false;
