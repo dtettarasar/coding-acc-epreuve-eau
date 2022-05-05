fe4d07ec1653b352671137848bc1f04868e9a4f4
Dylan Tettarasar <dtettarasar@gmail.com>
2022-05-05 22:25:49 +0200

    created eau10

---
+++ eau10.js
@@ -0,0 +1,32 @@
+// Index Wanted
+
+const argTester = () => {
+
+  const arguments = process.argv.slice(2);
+
+  if (arguments.length < 2) {
+
+    console.log("Veuillez passez au moins deux chaînes de caractère en argument.");
+    console.log("Exemple : node eau10.js Killing Is My Business and Business Is Good");
+
+    return false;
+
+  }
+
+  return arguments;
+
+}
+
+const main = () => {
+
+  const arguments = argTester();
+
+  if (arguments) {
+
+    console.log(arguments);
+
+  }
+
+}
+
+main();
