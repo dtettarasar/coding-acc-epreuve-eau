826351a0cb9f28cfb1fa2726bac39289669eb662
Dylan Tettarasar <dtettarasar@gmail.com>
2022-05-07 05:47:24 -0400

    eau11 : update in argTester

--- eau11.js
+++ eau11.js
@@ -5,13 +5,26 @@ const argTester = () => {
     const arguments = process.argv.slice(2);
     const pattern = /^-?[0-9]+$/;
     const errorMsg = ("Veuillez passer une série de nombres entiers en argument (au moins deux nombres).\nExemple: node eau11.js 27 6 12 13");
+    const data = [];
     
     if (arguments.length < 2) {
         console.log(errorMsg);
         return false;
     }
 
+    for (let i = 0; i < arguments.length; i++) {
+
+        if (!pattern.test(arguments[i])) {
+            console.log(errorMsg);
+            return false;
+        } else {
+            data.push(parseInt(arguments[i]));
+        }
+
+    }
+
     console.log(arguments);
+    console.log(data);
 
 }
 
