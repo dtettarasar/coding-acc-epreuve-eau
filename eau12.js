daaeafc1a7f767fa0db8df871b6fecb772a54ace
Dylan Tettarasar <dtettarasar@gmail.com>
2022-05-10 11:58:33 -0400

    created eau12

---
+++ eau12.js
@@ -0,0 +1,34 @@
+// Tri à bulle 
+
+const argTester = () => {
+
+    const arguments = process.argv.slice(2);
+    const pattern = /^-?[0-9]+$/;
+    const errorMsg = ("Veuillez passer une série de nombres entiers en argument (au moins deux nombres).\nExemple: node eau12.js 27 6 12 13");
+    const data = [];
+
+    if (arguments.length < 2) {
+        console.log(errorMsg);
+        return false;
+    }
+
+    for (let i = 0; i < arguments.length; i++) {
+
+        if (!pattern.test(arguments[i])) {
+            console.log(errorMsg);
+            return false;
+        } else {
+
+            const int = parseInt(arguments[i]) < 0 ? parseInt(arguments[i]) * -1 : parseInt(arguments[i]);
+            data.push(int);
+
+        }
+
+    }
+
+    return data;
+
+}
+
+console.log(argTester());
+
