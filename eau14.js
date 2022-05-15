0eb699579c0a4578cd8fb892283e20c211d07b1e
Dylan Tettarasar <dtettarasar@gmail.com>
2022-05-15 17:41:48 +0200

    created eau14

---
+++ eau14.js
@@ -0,0 +1,19 @@
+// Par ordre Ascii
+
+const argTester = () => {
+
+    const arguments = process.argv.slice(2);
+
+    if (arguments.length < 2) {
+
+        console.log("Veuillez passer au moins deux chaînes de caractère en argument");
+        console.log("Exemple: node14.js Countdown to Extinction");
+
+        return false;
+    }
+
+    console.log(arguments);
+
+}
+
+argTester();
